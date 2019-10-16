4468a1d689cc67d99845888abea78a820737b083
VirajSingh19 <virajsingh19@yahoo.com>
2019-10-16 18:35:31 +0530

    deleted src directory

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="main">
+        App.js
+      </div>
+    );
+  }
+}
+
+export default App;
