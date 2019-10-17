01f22351e3be6b2167c984ffc839f8d8977a246a
VirajSingh19 <virajsingh19@yahoo.com>
2019-10-17 20:09:00 +0530

    Updates

---
+++ precache-manifest.0e95466f028d933207c57c81563cb402.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8e2fc31dfc6dd1dab3cc97103059c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6056c2314ce9883dcbf",
+    "url": "/static/css/2.70842c70.chunk.css"
+  },
+  {
+    "revision": "aae7bb7596572b1f8841",
+    "url": "/static/css/main.7e6ea18f.chunk.css"
+  },
+  {
+    "revision": "b6056c2314ce9883dcbf",
+    "url": "/static/js/2.7883b231.chunk.js"
+  },
+  {
+    "revision": "aae7bb7596572b1f8841",
+    "url": "/static/js/main.8fdf3308.chunk.js"
+  },
+  {
+    "revision": "0f46f628ad36bcf8a629",
+    "url": "/static/js/runtime-main.d4cde748.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
