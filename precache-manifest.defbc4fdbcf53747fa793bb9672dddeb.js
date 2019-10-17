ccb9074ee5f05a7fb1a517848713617cdaa73308
VirajSingh19 <virajsingh19@yahoo.com>
2019-10-17 20:11:50 +0530

    Updates

---
+++ precache-manifest.defbc4fdbcf53747fa793bb9672dddeb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe2388ae72b7f3399b37d446fe8fd3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "413a58443cde2ddac21d",
+    "url": "./static/css/2.3eef924d.chunk.css"
+  },
+  {
+    "revision": "aae7bb7596572b1f8841",
+    "url": "./static/css/main.7e6ea18f.chunk.css"
+  },
+  {
+    "revision": "413a58443cde2ddac21d",
+    "url": "./static/js/2.7883b231.chunk.js"
+  },
+  {
+    "revision": "aae7bb7596572b1f8841",
+    "url": "./static/js/main.8fdf3308.chunk.js"
+  },
+  {
+    "revision": "a57a8057a3ec41fd1197",
+    "url": "./static/js/runtime-main.ed9e0b36.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e95466f028d933207c57c81563cb402.js"
+  "./precache-manifest.defbc4fdbcf53747fa793bb9672dddeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
